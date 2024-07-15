e3e9aca97af7218ad9f658f106a5e4cd38334fb5
Mariojung123 <mario@kyonggi.ac.kr>
2024-07-15 13:45:11 +0900

    파일을 위한 기타 설정 및 실행파일들

---
+++ backend/src/main/java/com/example/KEBProject/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.KEBProject.controller;
+
+import com.example.KEBProject.entity.User;
+import com.example.KEBProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/joinform")
+    public String join() {
+        return "join"; // join.html 템플릿을 반환
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        // Checkbox 처리
+        if (user.getIsExpert() == null) {
+            user.setIsExpert(false);
+        }
+        userService.createUser(user);
+        return "redirect:/users/joinform";
+    }
+}
