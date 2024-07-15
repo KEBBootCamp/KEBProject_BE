c7dfcaab1c0d0be0f6400ed1f72c067095f2e080
Mariojung123 <mario@kyonggi.ac.kr>
2024-07-15 13:43:35 +0900

    JPA로 DB 설계 완성및 테스트 완료

--- backend/src/main/java/com/example/KEBProject/repository/ExpertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.KEBProject.repository;
-
-import com.example.KEBProject.entity.Expert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpertRepository extends JpaRepository<Expert, String> {
-}
--- backend/src/main/java/com/example/KEBProject/repository/InspectionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.KEBProject.repository;
-
-import com.example.KEBProject.entity.Inspection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InspectionRepository extends JpaRepository<Inspection, Integer> {
-}
