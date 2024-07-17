171f727ddc47b67e3bf972c8c5ee51bd03c4664a
Mariojung123 <mario@kyonggi.ac.kr>
2024-07-17 16:07:34 +0900

    login기능 구현 및 세션 유지

--- backend/src/main/java/com/example/KEBProject/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.KEBProject.repository;
-
-import com.example.KEBProject.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
