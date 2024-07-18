92bb5f93f84797532ecd2cc2c80cb02706da0910
yonggi1234 <littedeer108@gmail.com>
2024-07-18 13:30:08 +0900

    UserId 검색 구현

---
+++ backend/src/main/java/com/example/KEBProject/repository/ExpertListRepository.java
@@ -0,0 +1,12 @@
+package com.example.KEBProject.repository;
+
+
+import com.example.KEBProject.entity.Expert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface ExpertListRepository extends JpaRepository<Expert, String> {
+    Optional<Expert> findByUserUserId(String userId);
+}
