27a208ed90f61ca840bb2fd716e3ed4f3ad4cf15
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2024-07-18 11:25:53 +0900

    Add files via upload

---
+++ backend/src/main/java/com/example/KEBProject/dto/InspectionDTO.java
@@ -0,0 +1,21 @@
+package com.example.KEBProject.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class InspectionDTO {
+  private Integer matchingId;
+  private String customerId;
+  private String engineerId;
+  private String model;
+  private String place;
+  private Timestamp inspectDate;
+  private Boolean checked;
+  private Boolean complete;
+  private Boolean isRate;
+}
