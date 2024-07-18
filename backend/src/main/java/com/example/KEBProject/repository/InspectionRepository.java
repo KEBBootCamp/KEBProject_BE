35e5295956f99dc0859af000ac495f726a969d69
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2024-07-18 11:28:17 +0900

    Add files via upload

---
+++ backend/src/main/java/com/example/KEBProject/repository/InspectionRepository.java
@@ -0,0 +1,13 @@
+package com.example.KEBProject.repository;
+
+import com.example.KEBProject.entity.Inspection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InspectionRepository extends JpaRepository<Inspection, Integer> {
+
+  List<Inspection> findByEngineerId(String engineerId);
+
+  List<Inspection> findByCustomerId(String customerId);
+}
