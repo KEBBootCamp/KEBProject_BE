0814c48dca6334c956794f58111eb15cfb202454
SeoGyeongWon <126853734+SeoGyeongWon@users.noreply.github.com>
2024-07-18 11:29:28 +0900

    Add files via upload

---
+++ backend/src/main/java/com/example/KEBProject/service/UserService.java
@@ -0,0 +1,39 @@
+package com.example.KEBProject.service;
+
+import com.example.KEBProject.entity.User;
+import com.example.KEBProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> showUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user); // 저장된 User 객체 반환
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public User findById(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        return user.orElse(null);
+    }
+
+    public User getUserById(String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        return userOptional.orElse(null);
+    }
+}
