9d7b8f01d3c8b5bfaae5bc2de631c2a7af3bc953
yonggi1234 <littedeer108@gmail.com>
2024-07-22 09:57:19 +0900

    Expert List 기능 구현

--- backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
+++ backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
@@ -3,10 +3,7 @@ package com.example.KEBProject.controller;
 
 
 import com.example.KEBProject.dto.ExpertDTO;
-import com.example.KEBProject.entity.User;
 import com.example.KEBProject.service.ExpertListService;
-import com.example.KEBProject.service.ExpertService;
-import com.example.KEBProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,21 +19,6 @@ public class SearchlistController {
     @Autowired
     private ExpertListService expertListService;
 
-    @GetMapping("/{engineerId}")
-    public String expertInfo(@PathVariable String engineerId, Model model) {
-        try {
-            ExpertDTO expertDto = expertListService.findByUserId(engineerId);
-            model.addAttribute("expertDTO", expertDto);
-            if (expertDto == null) {
-                return "expertNotFound";
-            }
-            return "expertInfo";
-        } catch (Exception e) {
-
-            e.printStackTrace();
-            return "errorPage";
-        }
-    }
     @GetMapping("/list")
     public String expertList(Model model){
         List<ExpertDTO> expertDto = expertListService.showExpertsDto();
