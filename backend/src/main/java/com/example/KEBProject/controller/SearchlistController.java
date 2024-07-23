8e73ae4f423dc133277b9448d6d48e27c89de291
yonggi1234 <littedeer108@gmail.com>
2024-07-23 15:23:15 +0900

    엔지니어 이름을 통한 상세페이지 연결

--- backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
+++ backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 @Controller
 @RequestMapping("/expert")
 public class SearchlistController {
@@ -17,8 +17,16 @@ public class SearchlistController {
     @Autowired
     private ExpertListService expertListService;
 
+    @GetMapping("/list")
+    public String expertList(Model model){
+        List<ExpertDTO> expertDto = expertListService.showExpertsDto();
+        model.addAttribute("expertDto", expertDto);
+        return "expertList";
+    }
+
+
     @GetMapping("/{engineerId}")
-    public String expertInfo(@PathVariable String engineerId, Model model) {
+    public String expertInfo(@PathVariable("engineerId") String engineerId, Model model) {
         try {
             ExpertDTO expertDto = expertListService.findByUserId(engineerId);
             model.addAttribute("expertDTO", expertDto);
