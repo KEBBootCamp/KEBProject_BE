2328e642cbb91427c5b2db0256d0db20a1c6dfa6
yonggi1234 <littedeer108@gmail.com>
2024-07-23 13:29:19 +0900

    주석 정리

--- backend/src/main/java/com/example/KEBProject/service/InspectionService.java
+++ backend/src/main/java/com/example/KEBProject/service/InspectionService.java
@@ -1,28 +1,22 @@
 package com.example.KEBProject.service;
 
-import com.example.KEBProject.entity.User;
 import com.example.KEBProject.repository.InspectionRepository;
 import com.example.KEBProject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.KEBProject.entity.Inspection;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
 import java.util.List;
-
 
 @Service
 public class InspectionService {
 
 
   private final InspectionRepository inspectionRepository;
-  private final UserRepository userRepository;
 
   @Autowired
   public InspectionService(InspectionRepository inspectionRepository, UserRepository userRepository) {
     this.inspectionRepository = inspectionRepository;
-    this.userRepository = userRepository;
   }
 
 
@@ -36,19 +30,6 @@ public class InspectionService {
     return inspectionRepository.findByCustomerId(customerId);
   }
 
-  //검수 신청부분
-  @Transactional
-  public Inspection createInspection(String userId, String model, String place, Timestamp inspectDate) {
-
-    Inspection inspection = new Inspection();
-    inspection.setModel(model);
-    inspection.setPlace(place);
-    inspection.setInspectDate(inspectDate);
-
-    inspection.setCustomerId(userId);
-
-    return inspectionRepository.save(inspection);
-  }
 
   //inspection_submit페이지를 위한 service
   public Inspection getInspectionById(int matchingId) {
