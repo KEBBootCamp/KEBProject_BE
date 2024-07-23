84acd3749cdaff33f546817ecdcc90183d822c9c
yonggi1234 <littedeer108@gmail.com>
2024-07-23 13:30:06 +0900

    model, place 전달받음

--- backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
+++ backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
@@ -3,13 +3,19 @@ package com.example.KEBProject.controller;
 
 
 import com.example.KEBProject.dto.ExpertDTO;
+import com.example.KEBProject.dto.InspectionDTO;
 import com.example.KEBProject.service.ExpertListService;
+import com.example.KEBProject.service.InspectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -18,12 +24,40 @@ public class SearchlistController {
 
     @Autowired
     private ExpertListService expertListService;
+    @Autowired
+    private InspectionService inspectionService;
 
-    @GetMapping("/list")
+    @PostMapping("/list")
     public String expertList(Model model){
-        List<ExpertDTO> expertDto = expertListService.showExpertsDto();
+
+        List<ExpertDTO> expertDto = expertListService.findAllExpertsDto();
         model.addAttribute("expertDto", expertDto);
+
+
         return "expertList";
     }
+
+
+    @PostMapping("/insepctionInfo")
+    public String submitInspectionForm(@RequestParam("customerId") String customerId,
+                                       @RequestParam("model") String model,
+                                       @RequestParam("place") String place,
+                                       @RequestParam("inspectDateTime") String inspectDateTime,
+                                       Model inspectionModel) {
+        if (inspectDateTime == null || inspectDateTime.isEmpty()) {
+            return "expertNotFound"; // 검수 날짜가 입력되지 않았을 경우 처리
+        }
+
+        LocalDate localDate = LocalDate.parse(inspectDateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+        Timestamp timestamp = Timestamp.valueOf(localDate.atStartOfDay());
+
+        inspectionModel.addAttribute("customerId", customerId);
+        inspectionModel.addAttribute("model", model);
+        inspectionModel.addAttribute("place", place);
+        inspectionModel.addAttribute("inspectDate", inspectDateTime);
+
+        return "show";
+    }
+
 }
 
