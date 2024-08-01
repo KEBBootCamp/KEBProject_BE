991a9b3d87520b370b2fb539f463ef97f3a0fb9a
yonggi1234 <littedeer108@gmail.com>
2024-08-01 14:40:00 +0900

    brand, DateTime 컬럼 추가

--- backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
+++ backend/src/main/java/com/example/KEBProject/controller/SearchlistController.java
@@ -4,14 +4,17 @@ package com.example.KEBProject.controller;
 
 import com.example.KEBProject.dto.ExpertDTO;
 import com.example.KEBProject.dto.InspectionDTO;
+import com.example.KEBProject.entity.Inspection;
 import com.example.KEBProject.entity.User;
 import com.example.KEBProject.service.ExpertListService;
+import com.example.KEBProject.service.InspectionService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 @Controller
 @RequestMapping("/expert")
@@ -19,6 +22,8 @@ public class    SearchlistController {
 
     @Autowired
     private ExpertListService expertListService;
+    @Autowired
+    private InspectionService inspectionService;
 
     //TC-5 엔지니어 목록
     @GetMapping("/list")
@@ -36,16 +41,47 @@ public class    SearchlistController {
     public String expertInfo(@RequestParam("userId") String userId,
                              @RequestParam("customerId") String customerId,
                              @RequestParam("model") String model,
+                             @RequestParam("brand") String brand,
                              @RequestParam("place") String place,
+                             @RequestParam("inspectDateTime") Timestamp inspectDateTime,
                              HttpSession session,  Model inspectionmodel) {
         User user = (User) session.getAttribute("user");
 
         inspectionmodel.addAttribute("customerId",user.getUserId());
         inspectionmodel.addAttribute("expertId",userId  );
         inspectionmodel.addAttribute("model",model);
+        inspectionmodel.addAttribute("brand",brand);
         inspectionmodel.addAttribute("place",place);
 
+        inspectionmodel.addAttribute("inspectDateTime",inspectDateTime);
+
+
+
         return "expertInfo";
+    }
+
+    @PostMapping("/createInspection")
+    public String createInspection(@RequestParam("userId") String userId,
+                                   @RequestParam("customerId") String customerId,
+                                   @RequestParam("model") String model,
+                                   @RequestParam("brand") String brand,
+                                   @RequestParam("place") String place,
+                                   @RequestParam("inspectDateTime") Timestamp inspectDateTime,
+                                   HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        Inspection inspection = new Inspection();
+
+        inspection.setCustomerId(user.getUserId());
+        inspection.setEngineerId(userId);
+        inspection.setModel(model);
+        inspection.setBrand(brand);
+        inspection.setPlace(place);
+        inspection.setInspectDate(inspectDateTime);
+//        inspection.setChecked(false);   검수 신청 시에는 null
+        inspection.setComplete(false);
+
+        inspectionService.createInspection(inspection);
+        return "redirect:/mypage";
     }
 
 }
