f9286c693d548d09872f22e7607999dc6e7302f0
yonggi1234 <littedeer108@gmail.com>
2024-08-05 15:55:36 +0900

    검수 조건 값 json 리턴

--- backend/src/main/java/com/example/KEBProject/controller/InspectionController.java
+++ backend/src/main/java/com/example/KEBProject/controller/InspectionController.java
@@ -14,20 +14,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-  import java.sql.Timestamp;
-  import java.time.LocalDate;
-  import java.time.LocalDateTime;
-  import java.time.format.DateTimeFormatter;
-  import java.util.ArrayList;
-  import java.util.HashMap;
-  import java.util.List;
-  import java.util.Map;
 
 @Controller
 public class InspectionController {
@@ -43,35 +32,39 @@ public class InspectionController {
     this.expertService =  expertService;
   }
 
-    //TC-4 검수 조건 검색
-    @GetMapping("/matching")
-    public String showInspectionForm(Model model, HttpSession session) {
-      //세션에서 가져옴
-      User currentUser = (User) session.getAttribute("user");
-
-      if (currentUser == null) {
-        // 유저가 null일 경우에 대한 처리 (예: 로그인 페이지로 리다이렉트)
-        return "/login";
-      }
-
-      model.addAttribute("userId", currentUser.getUserId());
-
-      //엔지니어 검수 수락 리스트
-      if (currentUser.getIsExpert()) {
-        List<Inspection> inspections = inspectionService.getInspectionsForExpert(currentUser.getUserId());
-        List<Inspection> filteredInspections = inspections.stream()
-            .filter(inspection -> inspection.getChecked() == null || inspection.getChecked())
-            .toList();
-        model.addAttribute("inspections", filteredInspections);
-        return "inspection_detail";
-      }
-      //엔지니어 프로필 리스트
-      else {
-        List<Inspection> inspections = inspectionService.getInspectionsForCustomer(currentUser.getUserId());
-        model.addAttribute("inspections", inspections);
-        return "inspection_form";
-      }
-    }
+  //TC-4 검수 조건 검색
+//  @GetMapping("/matching")
+//  public String showInspectionForm(Model model, HttpSession session) {
+//    //세션에서 가져옴
+//    User currentUser = (User) session.getAttribute("user");
+//
+//    if (currentUser == null) {
+//      // 유저가 null일 경우에 대한 처리 (예: 로그인 페이지로 리다이렉트)
+//      return "/login";
+//    }
+//
+//    model.addAttribute("userId", currentUser.getUserId());
+//
+//    //엔지니어 검수 수락 리스트
+//    if (currentUser.getIsExpert()) {
+//      List<Inspection> inspections = inspectionService.getInspectionsForExpert(currentUser.getUserId());
+//
+//      //검수 요청 응답한 것은 목록에서 사라지도록 설정
+//      List<Inspection> acceptInspections = inspections.stream()
+//          .filter(inspection -> inspection.getChecked() == null)
+//          .toList();
+//      model.addAttribute("inspections", acceptInspections);
+//
+//      return "engineer_requested";
+//
+//    }
+//    //엔지니어 프로필 리스트
+//    else {
+//      List<Inspection> inspections = inspectionService.getInspectionsForCustomer(currentUser.getUserId());
+//      model.addAttribute("inspections", inspections);
+//      return "inspection_form";
+//    }
+//  }
 
   //TC -4 검수 조건 검색 전달
   @PostMapping("/inspectionInfo")
@@ -108,79 +101,80 @@ public class InspectionController {
     redirectAttributes.addFlashAttribute("inspectionDTO", inspectionDTO);
     return "redirect:/expert/list";
   }
+  // 엔지니어 검수 요청 상세보기
+  @GetMapping("/inspection_detail/{matchingId}")
+  public String requestInspection(@PathVariable int matchingId, Model model) {
+    Inspection inspection = inspectionService.getInspectionById(matchingId);
 
-    // 엔지니어 검수 요청 상세보기
-    @GetMapping("/inspection_detail/{matchingId}")
-    public String requestInspection(@PathVariable int matchingId, Model model) {
-      Inspection inspection = inspectionService.getInspectionById(matchingId);
+    if (inspection != null) {
+      model.addAttribute("inspection", inspection);
+      return "inspection_detail";
+    } else {
+      return "redirect:/mypage";
+    }
+  }
 
-      if (inspection != null) {
-        model.addAttribute("inspection", inspection);
-        return "inspection_detail";
-      } else {
-        return "redirect:/mypage";
-      }
+  // 엔지니어 검수 수락 여부
+  @PostMapping("/matching/request/accept/{matchingId}")
+  @ResponseBody
+  public Map<String, String> checkInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
+
+    Inspection inspection = inspectionService.getInspectionById(matchingId);
+    if (inspection != null){
+      inspectionService.updateInspectionChecked(matchingId, true);
+
+      return Map.of("status","accept");
     }
 
-    // 엔지니어 검수 수락 여부
-    @PostMapping("/matching/request/accept/{matchingId}")
-    @ResponseBody
-    public Map<String, String> checkInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
-
-      Inspection inspection = inspectionService.getInspectionById(matchingId);
-      if (inspection != null){
-        inspectionService.updateInspectionChecked(matchingId, true);
-
-        return Map.of("status","accept");
-      }
-
-      return Map.of("status","failed");
-
-    }
-
-    //엔지니어 검수 요청 거절
-    @PostMapping("/matching/request/reject/{matchingId}")
-    @ResponseBody
-    public Map<String, String> rejectInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
-      Inspection inspection = inspectionService.getInspectionById(matchingId);
-      if (inspection != null){
-        inspectionService.updateInspectionChecked(matchingId, false);
-        return Map.of("status","rejected");
-      }
-
-      return Map.of("status","failed");
-    }
-
-
-    //엔지니어 검수 완료(Complete)
-    @PostMapping("/matching/request/complete/{matchingId}")
-    @ResponseBody
-    public Map<String, String> completeInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
-
-      Inspection inspection = inspectionService.getInspectionById(matchingId);
-      if (inspection != null){
-        inspectionService.updateInspectionComplete(matchingId, true);
-
-        return Map.of("status","complete");
-      }
-
-      return Map.of("status","failed");
-
-    }
-
-    //검수완료된 목록 고객이 확인
-    @GetMapping("/matching/end")
-    public String showEndRequested(Model model , HttpSession session) {
-      User currentUser = (User) session.getAttribute("user");
-
-      if (currentUser == null) {
-        return "redirect:login";
-      }
-
-      List<Inspection> endInspections = inspectionService.getEndInspection(currentUser.getUserId());
-      model.addAttribute("inspections", endInspections);
-
-      return "end_requested";
-    }
+    return Map.of("status","failed");
 
   }
+
+  //엔지니어 검수 요청 거절
+  @PostMapping("/matching/request/reject/{matchingId}")
+  @ResponseBody
+  public Map<String, String> rejectInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
+    Inspection inspection = inspectionService.getInspectionById(matchingId);
+    if (inspection != null){
+      inspectionService.updateInspectionChecked(matchingId, false);
+      return Map.of("status","rejected");
+    }
+
+    return Map.of("status","failed");
+  }
+
+
+  //엔지니어 검수 완료(Complete)
+  @PostMapping("/matching/request/complete/{matchingId}")
+  @ResponseBody
+  public Map<String, String> completeInspection(@PathVariable int matchingId, @RequestBody Map<String,Boolean> request) {
+
+    Inspection inspection = inspectionService.getInspectionById(matchingId);
+    if (inspection != null){
+      inspectionService.updateInspectionComplete(matchingId, true);
+
+      return Map.of("status","complete");
+    }
+
+    return Map.of("status","failed");
+
+  }
+
+  //검수완료된 목록 고객이 확인
+  @GetMapping("/matching/end")
+  public String showEndRequested(Model model , HttpSession session) {
+    User currentUser = (User) session.getAttribute("user");
+
+    if (currentUser == null) {
+      return "redirect:login";
+    }
+
+    List<Inspection> endInspections = inspectionService.getEndInspection(currentUser.getUserId());
+    model.addAttribute("inspections", endInspections);
+
+    return "end_requested";
+  }
+
+
+
+}
