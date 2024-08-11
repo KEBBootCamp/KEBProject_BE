55a4d85d7828ea0d39becaecfe6717a66274ac2f
Mariojung123 <mario@kyonggi.ac.kr>
2024-08-11 19:12:52 +0900

    Feat: Cors 및 기타 설정

---
+++ backend/src/main/java/com/example/KEBProject/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.KEBProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 로컬 React 앱의 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 자격 증명 허용
+    }
+}
